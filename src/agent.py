86a09abea13cf3899341e78e57aaea96a2fecb65
Le-Jr <nascimentoleandrojr@gmail.com>
2025-06-04 16:10:59 -0300

    feat: read csv

--- src/agent.py
+++ src/agent.py
@@ -1,4 +1,20 @@
 import pandas as pd
+from langchain_groq import ChatGroq
+from dotenv import load_dotenv
+import os
+import zipfile
 
-def main():
-    print("hello")
+base_dir = os.path.dirname(os.path.abspath(__file__))
+file_path = os.path.join(base_dir, 'data', '202401_NFs_Cabecalho.csv')
+
+# Forma de carregar as chaves do arquivo .env(Criado para ocultar chaves importantes)
+load_dotenv()
+
+
+
+# Instância para carregar o modelo e criar a conexão com a provedora do modelo
+llm = ChatGroq(model="llama3-70b-8192")
+
+df = pd.read_csv(file_path)
+print(df)
+
