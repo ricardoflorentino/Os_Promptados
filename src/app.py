1487cab9383acb2252c5cf9c2afaa064fff6bc76
Le-Jr <nascimentoleandrojr@gmail.com>
2025-06-04 21:55:06 -0300

    fix: adjust the loading on answer

--- src/app.py
+++ src/app.py
@@ -1,11 +1,16 @@
 import streamlit as st
 from src.agent import df_cabecalho, df_itens
 from src.agent import call_ai
+from langchain_core.messages import HumanMessage, AIMessage
+
 def main():
+    st.set_page_config(page_title="ChatBot para Nfes", page_icon="🤖")
+
     st.title("Agente de IA para Análises de notas fiscais - Os Promptados")
     st.write("## **Preview dos Datasets 📚**")
 
-    col1, col2 = col1, col2 = st.columns([3, 3])  # Porcentagens relativas das larguras
+    col1, col2 = st.columns([3, 3])  # Porcentagens relativas das larguras
+  
   
 
     with col1:
@@ -16,9 +21,17 @@ def main():
         st.write("#### Itens")
         st.write(df_itens)
 
+
     st.write("## Faça uma pergunta sobre as notas: ")
     question = st.text_input(placeholder="Faça uma pergunta sobre as notas fiscais...", label="Pergunta")
     if st.button("Fazer pergunta"):
         st.write("### Resposta")
-        answer = call_ai.analisar(question)
-        st.markdown(answer)
+        with st.spinner("🤔 Analisando sua pergunta"):
+            answer = call_ai.analisar(question)
+            st.markdown(answer)
+    
+
+
+
+
+    
