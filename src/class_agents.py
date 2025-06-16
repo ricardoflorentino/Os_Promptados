2a612e2776ada89a8f3da4198c0ee53f0f045c6d
Le-Jr <nascimentoleandrojr@gmail.com>
2025-06-16 14:39:32 -0300

    fix: solve output parsing errors

--- src/class_agents.py
+++ src/class_agents.py
@@ -1,16 +1,15 @@
 from dotenv import load_dotenv
 from langchain_experimental.agents.agent_toolkits import create_pandas_dataframe_agent
 from langchain_openai import ChatOpenAI
-from src.prompt import prefix, suffix
+from src.prompt import prefix_cabecalho, prefix_itens
 from langchain_groq import ChatGroq
 from src.dataframes import df_cabecalho, df_itens
-
+from langchain_core.output_parsers import StrOutputParser
 # Instância para carregar o modelo e criar a conexão com a provedora do modelo
 load_dotenv()
 
-
-llm = ChatGroq(model="llama-3.1-8b-instant") # deepseek-r1-distill-llama-70b llama-3.1-8b-instant llama-3.3-70b-versatile 
-# llm = ChatOpenAI(model="gpt-4-turbo", temperature=0)
+# llm = ChatGroq(model="llama-3.1-8b-instant") # deepseek-r1-distill-llama-70b llama-3.1-8b-instant  llama-3.3-70b-versatile
+llm = ChatOpenAI(model="gpt-4o", temperature=0.7)
 
 # Classe para configurar os agentes
 class agents:
@@ -20,9 +19,8 @@ class agents:
             verbose=True,
             allow_dangerous_code=True,
             max_iterations=5,
-            agent_type="zero-shot-react-description",
-            return_intermediate_steps=False,
-            prefix=prefix,
+            return_intermediate_steps=True,
+            prefix=prefix_cabecalho,
         )
     agent_itens = create_pandas_dataframe_agent(
         llm = llm,
@@ -30,6 +28,6 @@ class agents:
         verbose= True,
         allow_dangerous_code=True,
         max_iterations=5,
-        return_intermediate_steps=False,
-        prefix=prefix,
+        return_intermediate_steps=True,
+        prefix=prefix_itens,
     )
